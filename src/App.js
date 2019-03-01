2e9bf95f91f6d60d44d862ace50d41d20044e935
zhusijia3 <zhusijia3@jd.com>
2019-03-01 11:36:10 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import View from './components/swiper';
+import React, { Component, Fragment } from 'react';
+import Swiper from './components/swiper';
 import styles from './App.less';
 
 class App extends Component {
   render() {
     return (
-      <div className={styles['contain-wrapper']}>
-        <View />
-      </div>
+      <Fragment>
+        <div className={styles['contain-wrapper']}>
+          <Swiper width={500} height={200} />
+        </div>
+        <button type="button">上一张</button>
+        <button type="button">下一张</button>
+      </Fragment>
     );
   }
 }
--- src/components/swiper.js
+++ src/components/swiper.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import styles from './swiper.less';
-import Slider from './slider';
+// import Slider from './slider';
 import imageOne from '../image/angularjs.jpg';
 import imageTwo from '../image/react.png';
 import imageThree from '../image/vue.jpg';
 
 const imgArr = [imageOne, imageTwo, imageThree];
 
+const Slider = props => {
+  const { imgUrl } = props;
+  return <img className="slider" src={imgUrl} alt="pic" />;
+};
+
 class Swiper extends Component {
   render() {
+    const { width, height } = this.props;
+    const sliderStyle = {
+      width,
+      height
+    };
     return (
-      <div className={styles['swiper-wrapper']}>
-        <h1 className="fontStyle">Hello---React-Cli</h1>
-        {imgArr.map(item => (
-          <Slider imgUrl={item} />
+      <div className={styles['swiper-wrapper']} style={sliderStyle}>
+        {imgArr.map((item, idx) => (
+          <Slider imgUrl={item} key={idx + 1} />
         ))}
       </div>
     );
