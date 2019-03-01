3aaf0b88b8f8cd61d09e066148975319f9edb531
zhusijia3 <zhusijia3@jd.com>
2019-03-01 12:41:53 +0800

    update code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, { Component, Fragment } from 'react';
 import Swiper from './components/swiper';
 import styles from './App.less';
 
+import imageOne from './image/angularjs.jpg';
+import imageTwo from './image/react.png';
+import imageThree from './image/vue.jpg';
+
+const imgArr = [imageOne, imageTwo, imageThree];
+
 class App extends Component {
   render() {
     return (
       <Fragment>
         <div className={styles['contain-wrapper']}>
-          <Swiper width={500} height={200} />
+          <Swiper width={500} height={200} imgList={imgArr} autoPlay />
         </div>
         <button type="button">上一张</button>
         <button type="button">下一张</button>
--- src/components/swiper.js
+++ src/components/swiper.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
 import styles from './swiper.less';
-// import Slider from './slider';
-import imageOne from '../image/angularjs.jpg';
-import imageTwo from '../image/react.png';
-import imageThree from '../image/vue.jpg';
-
-const imgArr = [imageOne, imageTwo, imageThree];
 
 const Slider = props => {
   const { imgUrl } = props;
@@ -13,15 +7,43 @@ const Slider = props => {
 };
 
 class Swiper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentIndex: 0,
+      timer: null
+    };
+  }
+
+  componentDidMount() {
+    const { autoPlay } = this.props;
+    // const { timer } = this.state;
+    console.log(autoPlay);
+    if (autoPlay) {
+      this.timerId = setInterval(() => {
+        this.setCurrentIdx();
+      }, 2000);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  setCurrentIdx = () => {
+    const { currentIndex } = this.state;
+    console.log(currentIndex);
+  };
+
   render() {
-    const { width, height } = this.props;
+    const { width, height, imgList } = this.props;
     const sliderStyle = {
       width,
       height
     };
     return (
       <div className={styles['swiper-wrapper']} style={sliderStyle}>
-        {imgArr.map((item, idx) => (
+        {imgList.map((item, idx) => (
           <Slider imgUrl={item} key={idx + 1} />
         ))}
       </div>
